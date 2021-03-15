c0de99f2d646f068ea9ef20a8ac478da6540938d
Mprystal <mjprystal@gmail.com>
2021-03-15 16:57:26 -0400

    added loggers

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const articleRouter = require('./routes/articles');
 const {
   createUser, login,
 } = require('./controllers/users');
+const NotFoundError = require('./middleware/notFoundError');
 const auth = require('./middleware/auth');
+const { requestLogger, errorLogger } = require('./middleware/logger');
 
 mongoose.connect('mongodb://localhost:27017/articlesdb', {
   useNewUrlParser: true,
@@ -21,6 +23,8 @@ mongoose.connect('mongodb://localhost:27017/articlesdb', {
 });
 
 app.use(express.json());
+
+app.use(requestLogger);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -42,7 +46,22 @@ app.use(auth);
 app.use('/', userRouter);
 app.use('/', articleRouter);
 
+app.get('*', () => {
+  throw new NotFoundError('Requested resource not found');
+});
+
+app.use(errorLogger);
+
 app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode)
+    .send({
+      message: statusCode === 500 ? 'An error occurred on the server' : message,
+    });
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
