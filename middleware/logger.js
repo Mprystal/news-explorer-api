96ab9ddb2c3edca4d9dc84de10331bd8af7ad962
Mprystal <mjprystal@gmail.com>
2021-03-23 08:44:48 -0400

    fix winston loggs

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,16 +1,19 @@
 const winston = require('winston');
 const expressWinston = require('express-winston');
+const path = require('path');
+
+const logDir = 'logs';
 
 const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({ filename: '../logs/request.log' }),
+    new winston.transports.File({ filename: path.join(logDir, '/request.log') }),
   ],
   format: winston.format.json(),
 });
 
 const errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.File({ filename: '../logs/error.log' }),
+    new winston.transports.File({ filename: path.join(logDir, '/error.log') }),
   ],
   format: winston.format.json(),
 });
