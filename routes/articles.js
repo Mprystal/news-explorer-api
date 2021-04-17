1c29cb0f410145c5532e439a24bf25f812f0ec71
Mprystal <mjprystal@gmail.com>
2021-04-17 14:34:31 -0400

    switch date to string in joi

--- routes/articles.js
+++ routes/articles.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const { celebrate, Joi } = require('celebrate');
 
-const { getArticles, createArticle, deleteArticle } = require('../controllers/articles');
+const {
+  getArticles,
+  createArticle,
+  deleteArticle,
+} = require('../controllers/articles');
 
 const router = express.Router();
 
@@ -10,23 +14,31 @@ router.get('/articles', getArticles);
 
 // creates an article with the passed
 // keyword, title, text, date, source, link, and image in the body
-router.post('/articles', celebrate({
-  body: Joi.object().keys({
-    keyword: Joi.string().required(),
-    title: Joi.string().required(),
-    text: Joi.string().required(),
-    date: Joi.date().required(),
-    source: Joi.string().required(),
-    link: Joi.string().uri().required(),
-    image: Joi.string().uri().required(),
+router.post(
+  '/articles',
+  celebrate({
+    body: Joi.object().keys({
+      keyword: Joi.string().required(),
+      title: Joi.string().required(),
+      text: Joi.string().required(),
+      date: Joi.string().required(),
+      source: Joi.string().required(),
+      link: Joi.string().uri().required(),
+      image: Joi.string().uri().required(),
+    }),
   }),
-}), createArticle);
+  createArticle,
+);
 
 // # deletes the stored article by _id
-router.delete('/articles/:articleId', celebrate({
-  params: Joi.object().keys({
-    articleId: Joi.string().hex().length(24),
+router.delete(
+  '/articles/:articleId',
+  celebrate({
+    params: Joi.object().keys({
+      articleId: Joi.string().hex().length(24),
+    }),
   }),
-}), deleteArticle);
+  deleteArticle,
+);
 
 module.exports = router;
